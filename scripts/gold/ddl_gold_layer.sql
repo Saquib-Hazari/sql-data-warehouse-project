23e6d68212f3c330d1072921eecd81b4f72699be
Saquib Hazari <saquibhazari1000@gmail.com>
2025-04-06 14:11:47 +0530

    modified gold layer

--- scripts/gold/ddl_gold_layer.sql
+++ scripts/gold/ddl_gold_layer.sql
@@ -14,17 +14,19 @@ SELECT ROW_NUMBER() OVER(
   ci.cst_key AS customer_number,
   ci.cst_firstname AS customer_firstname,
   ci.cst_lastname AS customer_lastname,
+  cl.cntry AS country,
+  ci.cst_marital_status AS marital_status,
   CASE
-    WHEN ci.cst_gender != 'n/a' THEN ci.cst_gender -- created from the master table.
+    WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- created from the master table.
     ELSE COALESCE(ca.gen, 'n/a')
   END AS gender,
-  ci.cst_marital_status AS marital_status,
-  cl.cntry AS country,
   ca.bdate AS birth_date,
   ci.cst_create_date AS create_date
 FROM silver.crm_cust_info ci
   LEFT JOIN silver.erp_cust_az12 ca ON ca.cid = ci.cst_key
-  LEFT JOIN silver.erp_loc_a101 cl ON cl.cid = ci.cst_key -- Creating join for the prd_info and erp_prd_a12 table
+  LEFT JOIN silver.erp_loc_a101 cl ON cl.cid = ci.cst_key 
+
+-- Creating join for the prd_info and erp_prd_a12 table
 CREATE OR REPLACE VIEW gold.dim_products AS
 SELECT ROW_NUMBER() OVER(
     ORDER BY cp.prd_start_dt,
@@ -33,7 +35,7 @@ SELECT ROW_NUMBER() OVER(
   cp.prd_id AS product_id,
   cp.prd_key AS product_number,
   cp.prd_nm AS product_name,
-  cp.cst_id AS customer_id,
+  cp.cat_id AS customer_id,
   pc.cat AS category,
   pc.subcat AS subcategory,
   pc.maintenance AS maintenance,
@@ -41,8 +43,10 @@ SELECT ROW_NUMBER() OVER(
   cp.prd_line AS product_line,
   cp.prd_start_dt AS start_date
 FROM silver.crm_prd_info cp
-  LEFT JOIN silver.erp_px_cat_g1v2 pc ON pc.id = cp.cst_id
-WHERE prd_end_dt IS NULL -- Creating join for sales table with customer and product -- keys
+  LEFT JOIN silver.erp_px_cat_g1v2 pc ON pc.id = cp.cat_id
+WHERE prd_end_dt IS NULL
+
+-- Creating join for sales table with customer and product -- keys
 CREATE OR REPLACE VIEW gold.fact_sales AS
 SELECT sls_ord_num AS order_number,
   dp.product_key,
