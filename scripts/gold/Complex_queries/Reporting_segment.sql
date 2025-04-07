0c3a8735bd232380277236cefda3f9970171f78b
Saquib Hazari <saquibhazari1000@gmail.com>
2025-04-07 09:47:27 +0530

    Added new csv file to final report

--- scripts/gold/Complex_queries/Reporting_segment.sql
+++ scripts/gold/Complex_queries/Reporting_segment.sql
@@ -1,3 +1,25 @@
+/*
+======================================================
+Purpose: 
+  This is a Report consolidate key customers metrics and behavior.
+
+Highlight:
+  Gather essential field such as name, age, and transaction details.
+  - Segment customers into category (VIP, Regular, New) and age group.
+  - Aggregate customer level metrics.
+  - total orders
+  - total sales
+  - total quantity
+  - total produces
+  - total life span (month)
+
+  Calculate valuable KPIs:
+  - recency month since last order.
+
+========================================================
+*/
+
+-- (followed by your full query here)
 CREATE OR REPLACE VIEW gold.report_customers AS 
 WITH base_query AS (
   SELECT 
@@ -29,16 +51,27 @@ aggregated_metrics AS (
     SUM(quantity) AS total_quantity,
     COUNT(DISTINCT product_key) AS total_product,
     MAX(order_date) AS last_order_date,
-    DATE_PART('month', AGE(MAX(order_date), MIN(order_date))) 
-      + (DATE_PART('year', AGE(MAX(order_date), MIN(order_date))) * 12) AS lifespan,
-    DATE_PART('month', AGE(CURRENT_DATE, MAX(order_date))) 
-      + (DATE_PART('year', AGE(CURRENT_DATE, MAX(order_date))) * 12) AS recency_months
+    CAST(DATE_PART('month', AGE(MAX(order_date), MIN(order_date))) 
+      + (DATE_PART('year', AGE(MAX(order_date), MIN(order_date))) * 12) AS INTEGER) AS lifespan,
+    CAST(DATE_PART('month', AGE(CURRENT_DATE, MAX(order_date))) 
+      + (DATE_PART('year', AGE(CURRENT_DATE, MAX(order_date))) * 12) AS INTEGER) AS recency_months
   FROM base_query
   GROUP BY customer_key, customer_number, customer_name, age, country
 )
 
 SELECT 
-  *,
+  customer_key,
+  customer_number,
+  customer_name,
+  country,
+  age,
+  total_orders,
+  total_sales,
+  total_quantity,
+  total_product,
+  last_order_date,
+  lifespan,
+  recency_months,
   CASE 
     WHEN age < 20 THEN 'Below 20'
     WHEN age BETWEEN 20 AND 29  THEN '20 - 30'
@@ -51,14 +84,12 @@ SELECT
     WHEN lifespan > 12 AND total_sales <= 5000 THEN 'REGULAR'
     ELSE 'NEW'
   END AS customer_segment,
-  -- compute average order value
   CASE 
     WHEN total_sales = 0 THEN 0
     ELSE ROUND(total_sales / total_orders, 2)
   END AS avg_order_value,
-  -- compute avg monthly spend
   CASE 
     WHEN lifespan = 0 THEN total_sales  
-    ELSE  ROUND(total_sales::NUMERIC / lifespan::NUMERIC,2)
+    ELSE ROUND(total_sales::NUMERIC / lifespan::NUMERIC, 2)
   END AS avg_monthly_spent
-FROM aggregated_metrics
+FROM aggregated_metrics;
